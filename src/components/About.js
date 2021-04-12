bb5ed996cd19e2de8aeebec7fc0c15ac22003e9a
Daniel <dyankiver@gmail.com>
2021-04-12 17:42:07 -0400

    Added About Section

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+import headshot from "../my-photo.jpg";
+
+const About = () => {
+    return (
+        <div className="container py-5">
+            <div className="row">
+               <div className="col-lg-6 col-xm-12">
+                   <div className="photo-wrap mb-5">
+                       <img className="profile-img" src={headshot} alt="Headshot"/>
+                   </div>
+               </div>
+               <div className="col-lg-6 col-xm-12">
+                   <h1 className="about-heading">about me</h1>
+                   <p>
+                       Passionate full-stack web developer with experience building 
+                       applications with React, Javascript, Ruby, Rails, SQL databases, 
+                       CSS, HTML and RESTful API architecture. Innovative thinker with 
+                       a background in Film and TV Production. Programmer with strong 
+                       skills in team-building, project management, debugging, and code 
+                       optimization. Software Engineer with projects that incorporate 
+                       front-end and back-end development with robust user interfaces 
+                       (UI) that use efficient code to solve real-world problems.
+                   </p>
+               </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
