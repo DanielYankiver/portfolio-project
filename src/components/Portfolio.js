ccf6d5c70b897f28ab6f844556cd3f3c08ce2273
Daniel <dyankiver@gmail.com>
2021-04-13 15:15:26 -0400

    Finished first popup box in Projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,14 +6,48 @@ import taskManager from "../images/task-manager.png";
 // FONTAWESOME IMPORTS 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchPlus } from "@fortawesome/free-solid-svg-icons"
+// REACT POPUPBOX
+import {PopupboxManager, PopupboxContainer} from "react-popupbox"
+import "react-popupbox/dist/react-popupbox.css"
 
 const Portfolio = () => {
+
+    // Netflix
+    const openPopupboxNetflix = () => {
+        const content = (
+            <>
+                <img className="portfolio-image-popupbox" src={netflix} alt="Netflix Clone Project..."/>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Quasi ipsa sint quis. Iure velit incidunt corporis alias 
+                    aliquam repudiandae? Sed.
+                </p>
+                <b>Github:</b> 
+                <a className="hyper-link" onClick={() => window.open("https://github.com/DanielYankiver/rocket-fuel-frontend")}
+                >https://github.com/DanielYankiver/rocket-fuel-frontend</a>
+            </>
+        )
+        PopupboxManager.open({ content })
+    }
+
+    const popupboxConfigNetflix = {
+        titleBar: {
+            enable: true,
+            text: "Netflix Clone Project "
+        },
+        fadeIn: true,
+        fadeInSpeed: 500
+    }
+
+
+
+
     return (
         <div className="portfolio-wrapper">
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">portfolio</h1>
                 <div className="image-box-wrapper row justify-content-center">
-                    <div className="portfolio-image-box">
+                    <div className="portfolio-image-box" onClick={openPopupboxNetflix}>
                         <img className="portfolio-image" src={netflix} alt="Netflix Clone Project..." />
                         <div className="overflow"></div>
                         <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
@@ -38,6 +72,7 @@ const Portfolio = () => {
                     </div>
                 </div>
             </div>
+            <PopupboxContainer {...popupboxConfigNetflix} />
         </div>
     )
 }
