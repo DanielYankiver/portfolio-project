79ec535f98d4e2875456ca290cc740df0abd0561
Daniel <dyankiver@gmail.com>
2021-04-14 16:34:53 -0400

    Haflway Through Contact Section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Services from './components/Services';
 import Experience from './components/Experience';
 import Portfolio from './components/Portfolio';
 import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -41,6 +42,7 @@ function App() {
       <Experience />
       <Portfolio />
       <Testimonials />
+      <Contact />
     </>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,81 @@
+import React from 'react'
+
+const Contact = () => {
+
+
+    return (
+        <div className="contact">
+            <div className="text-center">
+                <h1>contact me</h1>
+                <p>
+                    please fill out the form and describe your project needs and 
+                    I'll contact you as soon as possible
+                </p>  
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 col-xs-12">
+                        {/* NAME INPUT */}
+                        <div className="text-center">
+                            <input
+                                type="text" 
+                                className="form-control"
+                                placeholder="Name"
+                                name="name"
+                            />
+                            <div className="line"></div>
+                        </div>
+                        {/* PHONE INPUT */}
+                        <div className="text-center">
+                            <input
+                                type="text"  
+                                className="form-control"
+                                placeholder="Phone Number"
+                                phone="phone"
+                            />
+                            <div className="line"></div>
+                        </div>
+                        {/* EMAIL INPUT */}
+                        <div className="text-center">
+                            <input
+                                type="email"  
+                                className="form-control"
+                                placeholder="Email"
+                                email="email"
+                            />
+                            <div className="line"></div>
+                        </div>
+                        {/* SUBJECT INPUT */}
+                        <div className="text-center">
+                            <input 
+                                type="text" 
+                                className="form-control"
+                                placeholder="Subject"
+                                subject="subject"
+                            />
+                            <div className="line"></div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-xs-12">
+                        {/* DESCRIPTION */}
+                        <div className="text-center">
+                            <textarea
+                                type="text" 
+                                className="form-control"
+                                placeholder="Please describe your project..."
+                                description="description"
+                            ></textarea>
+                            <div className="line"></div>
+                        </div>
+                        <button 
+                            className="btn-main-offer contact-btn" 
+                            type="submit"
+                        >contact me</button>
+                    </div>
+                </div>
+            </div>   
+        </div>
+    )
+}
+
+export default Contact
