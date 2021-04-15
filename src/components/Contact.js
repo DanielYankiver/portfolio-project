1b8c7275cf3f90bab39c0b976c7821d3db7521c2
Daniel <dyankiver@gmail.com>
2021-04-15 17:03:29 -0400

    Almost done with contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import emailjs from 'emailjs-com'
+import {useForm} from 'react-hook-form'
+// import * as useForm from "react-hook-form"
 
 const Contact = () => {
+    const [successMessage, setSuccessMessage] = useState("");
+    const { register, handleSubmit, errors } = useForm();
 
-    const serviceID = "service_ID";
-    const templateID = "template_ID";
+    const serviceID = "service_uz1sjk8";
+    const templateID = "template_kqeduka";
     const userID = "user_NiR06iHilhuwjmomgY6AS";
 
+    const onSubmit = (data, r) => {
+        sendEmail(
+            serviceID, 
+            templateID,
+            {
+                name: data.name,
+                phone: data.phone,
+                email: data.email,
+                subject: data.subject,
+                description: data.description
+            },
+            userID
+        )
+        r.target.reset();
+    }
 
-    function sendEmail(e) {
-        e.preventDefault();
-    
-        emailjs.sendForm(serviceID, templateID, e.target, userID)
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
+
+    const sendEmail = (serviceID, templateID, variables, userID) => {
+        emailjs.send(serviceID, templateID, variables, userID)
+          .then(() => {
+            setSuccessMessage("Form sent successfully! I'll contact you as soon as possible.")
+          }).catch(err => console.error(`Something went wrong ${err}`));
       }
 
 
@@ -30,19 +46,9 @@ const Contact = () => {
                 </p>  
             </div>
             <div className="container">
-                <form onSubmit={sendEmail}>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="row">
                         <div className="col-md-6 col-xs-12">
-                            {/* SUBJECT INPUT */}
-                            <div className="text-center">
-                                <input 
-                                    type="text" 
-                                    className="form-control"
-                                    placeholder="Subject"
-                                    name="subject"
-                                />
-                                <div className="line"></div>
-                            </div>
                             {/* NAME INPUT */}
                             <div className="text-center">
                                 <input
@@ -50,9 +56,21 @@ const Contact = () => {
                                     className="form-control"
                                     placeholder="Name"
                                     name="name"
+                                    // ref={
+                                    //    register({
+                                    //        required: "Please enter your name",
+                                    //        maxLength: {
+                                    //            value: 20,
+                                    //            message: "Please enter a name with fewer than 20 characters"
+                                    //        }
+                                    //    })
+                                    // }
                                 />
                                 <div className="line"></div>
                             </div>
+                            {/* <span className="error-message">
+                                {errors.name && errors.name.message}
+                            </span> */}
                             {/* PHONE INPUT */}
                             <div className="text-center">
                                 <input
@@ -70,6 +88,16 @@ const Contact = () => {
                                     className="form-control"
                                     placeholder="Email"
                                     name="email"
+                                />
+                                <div className="line"></div>
+                            </div>
+                            {/* SUBJECT INPUT */}
+                            <div className="text-center">
+                                <input 
+                                    type="text" 
+                                    className="form-control"
+                                    placeholder="Subject"
+                                    name="subject"
                                 />
                                 <div className="line"></div>
                             </div>
