4ecd316add6995155307791631d66f930eb2aa42
Daniel <dyankiver@gmail.com>
2021-04-21 17:17:19 -0600

    Changed logo and header to my name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
-                <h1>Web Development and Website Promotions </h1>
+                <h1>Daniel Yankiver</h1>
                 <Typed 
                     className="typed-text"
                     strings={["Software Engineer", "Full-Stack Developer", "Filmmaker", "React", "Javascript", "CSS", "Ruby on Rails"]}
