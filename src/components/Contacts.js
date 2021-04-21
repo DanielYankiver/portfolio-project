5becc1079cb46f1696b26b9a88a197048facaf76
Daniel <dyankiver@gmail.com>
2021-04-21 09:19:54 -0600

    Everything in Contact Form up to Validation

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,20 +1,23 @@
 import React from "react";
 import emailjs from "emailjs-com";
 
-const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-}
-
-// stopped here ^ 6:43 23. Contact Me Component Part #3
-
 const Contacts = () => {
+
+    const serviceID = "service_cgbaj5a"
+    const templateID = "template_yvesyfu"
+    const userID = "user_NiR06iHilhuwjmomgY6AS"
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm(serviceID, templateID, e.target, userID)
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
+    
     return (
         <div className="contacts">
            <div className="text-center">
@@ -22,63 +25,65 @@ const Contacts = () => {
                <p>Please fill out the form and describe your project needs and I'll contact you as soon as possible.</p>
            </div>
            <div className="container">
-               <div className="row">
-                   <div className="col-md-6 col-xs-12">
-                       {/* NAME INPUT */}
-                       <div className="text-center">
-                        <input
-                            type="text" 
-                            className="form-control"
-                            placeholder="Name"
-                            name= "name"
-                        />
-                        <div className="line"></div>
-                       </div>
-                       {/* PHONE INPUT */}
-                       <div className="text-center">
-                        <input
-                            type="text"  
-                            className="form-control"
-                            placeholder="Phone Number"
-                            name= "phone"
-                        />
-                        <div className="line"></div>
-                       </div>
-                       {/* EMAIL INPUT */}
-                       <div className="text-center">
-                        <input
-                            type="email"  
-                            className="form-control"
-                            placeholder="Email"
-                            name= "email"
-                        />
-                        <div className="line"></div>
-                       </div>
-                       {/* SUBJECT INPUT */}
-                       <div className="text-center">
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            placeholder="Subject"
-                            name= "subject"
-                        />
-                        <div className="line"></div>
-                       </div>
-                   </div>
-                   <div className="col-md-6 col-xs-12">
-                       {/* DESCRIPTION */}
-                       <div className="text-center">
-                        <textarea 
-                            type="text" 
-                            className="form-control"
-                            placeholder="Please describe your inquiry"
-                            name= "description"
-                        ></textarea>
-                        <div className="line"></div>
-                       </div>
-                       <button className="btn-main-offer contact-btn" type="submit">contact me</button>
-                   </div>
-               </div>
+               <form onSubmit={sendEmail}>
+                    <div className="row">
+                        <div className="col-md-6 col-xs-12">
+                            {/* NAME INPUT */}
+                            <div className="text-center">
+                                <input
+                                    type="text" 
+                                    className="form-control"
+                                    placeholder="Name"
+                                    name= "name"
+                                />
+                                <div className="line"></div>
+                            </div>
+                            {/* PHONE INPUT */}
+                            <div className="text-center">
+                                <input
+                                    type="text"  
+                                    className="form-control"
+                                    placeholder="Phone Number"
+                                    name= "phone"
+                                />
+                                <div className="line"></div>
+                            </div>
+                            {/* EMAIL INPUT */}
+                            <div className="text-center">
+                                <input
+                                    type="email"  
+                                    className="form-control"
+                                    placeholder="Email"
+                                    name= "email"
+                                />
+                                <div className="line"></div>
+                            </div>
+                            {/* SUBJECT INPUT */}
+                            <div className="text-center">
+                                <input 
+                                    type="text" 
+                                    className="form-control"
+                                    placeholder="Subject"
+                                    name= "subject"
+                                />
+                                <div className="line"></div>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-xs-12">
+                            {/* DESCRIPTION */}
+                            <div className="text-center">
+                                <textarea 
+                                    type="text" 
+                                    className="form-control"
+                                    placeholder="Please describe your inquiry"
+                                    name= "description"
+                                ></textarea>
+                                <div className="line"></div>
+                            </div>
+                            <button className="btn-main-offer contact-btn" type="submit">contact me</button>
+                        </div>
+                    </div>
+               </form>
            </div>
         </div>
     )
