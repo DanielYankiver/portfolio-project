7b3f874e65afb197d019a77db6ca04c566aa4f48
Daniel <dyankiver@gmail.com>
2021-04-21 18:51:52 -0600

    Updated Experince with info

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,14 +10,22 @@ const Experience = () => {
                 <div className="timeline-block timeline-block-right">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2014-2015</h3>
+                        <h3>Nov 2020 - Mar 2021</h3>
+                        <h2>Software Engineer</h2>
+                        <h2>Flatiron School</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing 
-                            elit. Ipsa vero laudantium perferendis odit nostrum 
-                            consequatur cupiditate veritatis ex alias eius, beatae 
-                            repellendus nesciunt iusto molestias adipisci maxime 
-                            placeat quidem accusamus quod deserunt doloribus 
-                            quaerat. Est voluptates id vel cumque tempore?
+                        Software Engineering Immersive Program designed to equip 
+                        students with the skills necessary to launch careers in 
+                        tech. This program includes three months of intensive, 
+                        full-time training some of this training included 
+                        classes/lectures, individual, pair and group programming 
+                        projects, code challenges, presentations and more.
+                        </p>
+                        <p>
+                            The curriculum encompasses a variety of languages, 
+                            frameworks, and libraries in-depth, such as Ruby, 
+                            Rails, SQL, PostgreSQL, JavaScript, React, Redux, 
+                            Git, HTML, and CSS.
                         </p>
                     </div>
                 </div>
@@ -25,29 +33,40 @@ const Experience = () => {
                 <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2015-2016</h3>
+                        <h3>Jun 2017 – Mar 2020</h3>
+                        <h2>Lead Content Producer (Digital)</h2>
+                        <h2>Sinclair Broadcast Group</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing 
-                            elit. Ipsa vero laudantium perferendis odit nostrum 
-                            consequatur cupiditate veritatis ex alias eius, beatae 
-                            repellendus nesciunt iusto molestias adipisci maxime 
-                            placeat quidem accusamus quod deserunt doloribus 
-                            quaerat. Est voluptates id vel cumque tempore?
+                            Lead the content development and digital division for 
+                            kids & family at Sinclair across all digital outlets 
+                            (web, mobile, apps).
                         </p>
+                        <p>
+                            Approved content for purchase, conceptualized digital 
+                            strategy, managed a team of developers, negotiated content 
+                            acquisitions and oversaw rollout of all digital platforms.
+                        </p>
+                        <p>
+                            Managed and maintained all digital outlets, while improving 
+                            retention and revenue across all platforms by 300%.
+                        </p>                    
                     </div>
                 </div>
                 {/* - */}
                 <div className="timeline-block timeline-block-right">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2016-2017</h3>
+                        <h3>Jun 2006 - Jul 2017</h3>
+                        <h2>Independent Filmmaker</h2>
+                        <h2>Director, Producer, Writer & Editor	</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing 
-                            elit. Ipsa vero laudantium perferendis odit nostrum 
-                            consequatur cupiditate veritatis ex alias eius, beatae 
-                            repellendus nesciunt iusto molestias adipisci maxime 
-                            placeat quidem accusamus quod deserunt doloribus 
-                            quaerat. Est voluptates id vel cumque tempore?
+                            Directed, produced, shot, edited and wrote award-winning 
+                            films, commercials,  music videos, documentaries and video 
+                            art. 
+                        </p>
+                        <p>
+                            Clients included production companies, arts foundations, 
+                            video platforms, distribution companies, etc.
                         </p>
                     </div>
                 </div>
@@ -55,44 +74,12 @@ const Experience = () => {
                 <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2017-2018</h3>
+                        <h3>Sept 2007- Jun 2011</h3>
+                        <h2>Bachelor of Fine Arts</h2>
+                        <h2>New York University</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing 
-                            elit. Ipsa vero laudantium perferendis odit nostrum 
-                            consequatur cupiditate veritatis ex alias eius, beatae 
-                            repellendus nesciunt iusto molestias adipisci maxime 
-                            placeat quidem accusamus quod deserunt doloribus 
-                            quaerat. Est voluptates id vel cumque tempore?
-                        </p>
-                    </div>
-                </div>
-                {/* - */}
-                <div className="timeline-block timeline-block-right">
-                    <div className="marker"></div>
-                    <div className="timeline-content">
-                        <h3>2018-2019</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing 
-                            elit. Ipsa vero laudantium perferendis odit nostrum 
-                            consequatur cupiditate veritatis ex alias eius, beatae 
-                            repellendus nesciunt iusto molestias adipisci maxime 
-                            placeat quidem accusamus quod deserunt doloribus 
-                            quaerat. Est voluptates id vel cumque tempore?
-                        </p>
-                    </div>
-                </div>
-                {/* - */}
-                <div className="timeline-block timeline-block-left">
-                    <div className="marker"></div>
-                    <div className="timeline-content">
-                        <h3>2019-2020</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing 
-                            elit. Ipsa vero laudantium perferendis odit nostrum 
-                            consequatur cupiditate veritatis ex alias eius, beatae 
-                            repellendus nesciunt iusto molestias adipisci maxime 
-                            placeat quidem accusamus quod deserunt doloribus 
-                            quaerat. Est voluptates id vel cumque tempore?
+                            - Tisch School of the Arts -
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus odit voluptatibus sed consequuntur? Officiis dolor est nemo mollitia quod dolore quam, ducimus inventore atque culpa, neque odio, asperiores dolores dicta!
                         </p>
                     </div>
                 </div>
