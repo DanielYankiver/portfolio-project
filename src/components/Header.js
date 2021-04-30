01d2b87f0313f80faa9cc4950abe689ae323780c
Daniel <dyankiver@gmail.com>
2021-04-30 14:09:07 -0400

    moved skills carousel to header compomnenent

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,30 @@ import React from "react"
 import Typed from "react-typed"
 import { Link } from "react-scroll";
 
+import SkillsCarousel from "./SkillsCarousel";
+
+
+
 const Header = () => {
     return (
-        <div className="header-wrapper">
-            <div id="home" className="main-info">
-                <h1>Daniel Yankiver</h1>
-                <Typed 
-                    className="typed-text"
-                    strings={["Software Engineer", "Filmmaker"]}
-                    typeSpeed={45}
-                    backSpeed={65}
-                    loop
-                />
-                <Link smooth={true} to="contact" offset={-110} className="btn-main-offer">Contact Me</Link>
+        <div>
+            <div className="header-wrapper">
+                <div id="home" className="main-info">
+                    <h1>Daniel Yankiver</h1>
+                    <Typed 
+                        className="typed-text"
+                        strings={["Software Engineer", "Filmmaker"]}
+                        typeSpeed={45}
+                        backSpeed={65}
+                        loop
+                    />
+                    <Link smooth={true} to="contact" offset={-110} className="btn-main-offer">Contact Me</Link>
+                </div>
+            </div>
+            <div className="container">
+                <div className="testimonials-content">
+                    <SkillsCarousel />
+                </div>
             </div>
         </div>
     )
--- src/components/SkillsCarousel.js
+++ src/components/SkillsCarousel.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import { FaReact } from "@react-icons/FaReact"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { FaReact } from 'react-icons/fa';
 import { SiJavascript, SiRails, SiCss3, SiRuby, SiHtml5, SiAdobepremiere, SiAdobephotoshop } from 'react-icons/si';
-
-
-// AVATAR IMPORTS
-import avatar1 from "../images/avatars/avatar-1.png";
-import avatar2 from "../images/avatars/avatar-2.png";
-import avatar3 from "../images/avatars/avatar-3.png";
-import avatar4 from "../images/avatars/avatar-4.png";
 
 const SkillsCarousel = () => {
     return (
@@ -24,99 +16,51 @@ const SkillsCarousel = () => {
                 interval={3000}
             >
                 <>
-                    {/* <img src={avatar1} alt="John Doe 1"/> */}
                     <div className="myCarousel">
                         <h3><FaReact /></h3>
                         <h3>React</h3>
-                        {/* <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Iste aspernatur distinctio quae nostrum laborum doloremque 
-                            assumenda excepturi, quibusdam omnis fuga!
-                        </p> */}
                     </div>
                 </>
                 <>
-                    {/* <img src={avatar2} alt="Jane Doe 2"/> */}
                     <div className="myCarousel">
                         <h3><SiJavascript /></h3>
                         <h3>Javascript</h3>
-                        {/* <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Iste aspernatur distinctio quae nostrum laborum doloremque 
-                            assumenda excepturi, quibusdam omnis fuga!
-                        </p> */}
                     </div>
                 </>
                 <>
-                    {/* <img src={avatar3} alt="John Doe 3"/> */}
                     <div className="myCarousel">
                         <h3><SiRuby /></h3>
                         <h3>Ruby</h3>
-                        {/* <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Iste aspernatur distinctio quae nostrum laborum doloremque 
-                            assumenda excepturi, quibusdam omnis fuga!
-                        </p> */}
                     </div>
                 </>
                 <>
-                    {/* <img src={avatar4} alt="John Doe 4"/> */}
                     <div className="myCarousel">
                         <h3><SiRails /></h3>
                         <h3>Ruby on Rails</h3>
-                        {/* <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Iste aspernatur distinctio quae nostrum laborum doloremque 
-                            assumenda excepturi, quibusdam omnis fuga!
-                        </p> */}
                     </div>
                 </>
                 <>
-                    {/* <img src={avatar4} alt="John Doe 4"/> */}
                     <div className="myCarousel">
                         <h3><SiCss3 /></h3>
                         <h3>CSS</h3>
-                        {/* <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Iste aspernatur distinctio quae nostrum laborum doloremque 
-                            assumenda excepturi, quibusdam omnis fuga!
-                        </p> */}
                     </div>
                 </>
                 <>
-                    {/* <img src={avatar4} alt="John Doe 4"/> */}
                     <div className="myCarousel">
                         <h3><SiHtml5 /></h3>
                         <h3>HTML</h3>
-                        {/* <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Iste aspernatur distinctio quae nostrum laborum doloremque 
-                            assumenda excepturi, quibusdam omnis fuga!
-                        </p> */}
                     </div>
                 </>
                 <>
-                    {/* <img src={avatar4} alt="John Doe 4"/> */}
                     <div className="myCarousel">
                         <h3><SiAdobepremiere /></h3>
                         <h3>Adobe Premiere</h3>
-                        {/* <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Iste aspernatur distinctio quae nostrum laborum doloremque 
-                            assumenda excepturi, quibusdam omnis fuga!
-                        </p> */}
                     </div>
                 </>
                 <>
-                    {/* <img src={avatar4} alt="John Doe 4"/> */}
                     <div className="myCarousel">
                         <h3><SiAdobephotoshop /></h3>
                         <h3>Adobe Photoshop</h3>
-                        {/* <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                            Iste aspernatur distinctio quae nostrum laborum doloremque 
-                            assumenda excepturi, quibusdam omnis fuga!
-                        </p> */}
                     </div>
                 </>
             </Carousel>
