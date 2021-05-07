2f16db274f1c653b7cd31bf55966224cb63d6991
Daniel <dyankiver@gmail.com>
2021-05-07 11:18:22 -0400

    added particle bubbles to contact page

--- src/App.js
+++ src/App.js
@@ -194,6 +194,60 @@ function App() {
       <Portfolio />
       <Filmmaker />
       {/* <Skills /> */}
+      <Particles 
+        className="particles-bubbles"
+        params={{
+          particles: {
+              number: {
+                  value: 160,
+                  density: {
+                      enable: false
+                  }
+              },
+              size: {
+                  value: 3,
+                  random: true,
+                  anim: {
+                      speed: 4,
+                      size_min: 0.3
+                  }
+              },
+              line_linked: {
+                  enable: false
+              },
+              move: {
+                  random: true,
+                  speed: 1,
+                  direction: "top",
+                  out_mode: "out"
+              }
+          },
+          interactivity: {
+              events: {
+                  onhover: {
+                      enable: true,
+                      mode: "bubble"
+                  },
+                  onclick: {
+                      enable: true,
+                      mode: "repulse"
+                  }
+              },
+              modes: {
+                  bubble: {
+                      distance: 250,
+                      duration: 2,
+                      size: 0,
+                      opacity: 0
+                  },
+                  repulse: {
+                      distance: 400,
+                      duration: 4
+                  }
+              }
+          }
+      }} 
+      />
       <Contact />
       <Footer />
     </>
@@ -201,3 +255,71 @@ function App() {
 }
 
 export default App;
+
+// PARTICLES POLYGON MASK 
+
+{/* <Particles
+params={{
+  fps_limit: 28,
+  particles: {
+      collisions: {
+          enable: false
+      },
+      number: {
+          value: 200,
+          density: {
+              enable: false
+          }
+      },
+      line_linked: {
+          enable: true,
+          distance: 30,
+          opacity: 0.4
+      },
+      move: {
+          speed: 1
+      },
+      opacity: {
+          anim: {
+              enable: true,
+              opacity_min: 0.05,
+              speed: 1,
+              sync: false
+          },
+          value: 0.4
+      }
+  },
+  polygon: {
+      enable: true,
+      scale: 0.5,
+      type: "inline",
+      move: {
+          radius: 10
+      },
+      url: "/small-deer.2a0425af.svg",
+      inline: {
+          arrangement: "equidistant"
+      },
+      draw: {
+          enable: true,
+          stroke: {
+              color: "rgba(255, 255, 255, .2)"
+          }
+      }
+  },
+  retina_detect: false,
+  interactivity: {
+      events: {
+          onhover: {
+              enable: true,
+              mode: "bubble"
+          }
+      },
+      modes: {
+          bubble: {
+              size: 6,
+              distance: 40
+          }
+      }
+  }
+}} /> */}
